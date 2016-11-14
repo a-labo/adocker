e18a5da77fb0910894e54faf0fd6e07b0e8a53a9
Taka Okunishi <okunishinishi@gmail.com>
2016-11-14 21:46:01 +0900

    Refactoring

---
+++ test/parse_args_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for parseArgs.
+ * Runs with mocha.
+ */
+'use strict'
+
+const parseArgs = require('../lib/helpers/parse_args.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('parse-args', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Parse args', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
