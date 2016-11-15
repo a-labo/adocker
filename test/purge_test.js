c9a152be0cccfb3efc9ac6974a4f17d00adced60
Taka Okunishi <okunishinishi@gmail.com>
2016-11-15 22:29:36 +0900

    Version incremented to 1.2.8

---
+++ test/purge_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for purge.
+ * Runs with mocha.
+ */
+'use strict'
+
+const purge = require('../lib/commands/purge.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('purge', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Purge', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
