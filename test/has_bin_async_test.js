ab26534245f2393d0f30d48f7afb58ca72002165
Taka Okunishi <okunishinishi@gmail.com>
2016-11-15 21:57:18 +0900

    Version incremented to 1.2.7

---
+++ test/has_bin_async_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for hasBinAsync.
+ * Runs with mocha.
+ */
+'use strict'
+
+const hasBinAsync = require('../lib/helpers/has_bin_async.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('has-bin-async', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Has bin async', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
