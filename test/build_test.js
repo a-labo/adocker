34768da9c55ddaf1fa9387ede8fa3392d7c890dd
Taka Okunishi <okunishinishi@gmail.com>
2016-11-15 13:04:33 +0900

    Version incremented to 1.2.3

---
+++ test/build_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for build.
+ * Runs with mocha.
+ */
+'use strict'
+
+const build = require('../lib/commands/build.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('build', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Build', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
